ec776be52983337e21046a6616b75908e28b47cf
wverastegui <wudmir.rojas@recetox.muni.cz>
2024-04-16 13:50:24 +0200

    Implemented unit test for load.lcms.R

---
+++ tests/testthat/test-load.lcms.R
@@ -0,0 +1,23 @@
+patrick::with_parameters_test_that(
+  "test load.lcms reads mzML file",
+  {
+    # Arrange: Set up test inputs
+    testdata <- file.path("..", "testdata")
+    input_path <- file.path(testdata, filename)
+
+    # Act: Execute the function with the test inputs
+    sut <- load.lcms(input_path)
+
+    # Assert: Verify the function output matches expected results
+    # Check that the function returns an object of the expected type
+    testthat::expect_s3_class(sut, "tbl_df")
+  },
+  patrick::cases(
+    test_case_1 = list(
+      filename = c("mbr_test0_copy.mzml")
+    ),
+    test_case_2 = list(
+      filename = c("mbr_test0_copy.mzxml")
+    )
+  )
+)

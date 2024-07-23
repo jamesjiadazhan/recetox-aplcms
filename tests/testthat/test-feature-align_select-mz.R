563389f505fc591dcec724199ae9f8afe3b1da7d
KristinaGomoryova <gomoryova@sci.muni.cz>
2024-07-23 08:49:33 +0200

    test updated for refactored code

--- tests/testthat/test-feature-align_select-mz.R
+++ tests/testthat/test-feature-align_select-mz.R
@@ -1,15 +1,16 @@
-test_that("select_mz function works", {
-  sample <- read_parquet("../testdata/input/feature-align_select-mz.parquet")
+test_that("create_features_from_cluster() function works", {
+  sample <- read_parquet("../testdata/input/feature-align_create-features.parquet")
   sample_names <- c("RCX_06_shortened", "RCX_07_shortened", "RCX_08_shortened")
   min_occurrence <- 2
   mz_tol_relative <- 6.85676325338646e-06
   rt_tol_relative <- 2.17918873407775
 
-  actual <- select_mz(sample,
+  actual <- create_features_from_cluster(sample,
                       mz_tol_relative,
                       rt_tol_relative,
                       min_occurrence,
                       sample_names)
-  expected <- readRDS("../testdata/aligned/output_select-mz.rds")
+  
+  expected <- readRDS("../testdata/aligned/output_create-features.rds")
   expect_equal(actual, expected)
 })
